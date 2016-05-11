22df1d9bfbd534ff3f1cf970fd35449e3504b7b6
Jonas Johannsen <jonas.johannsen@haw-hamburg.de>
2016-05-11 15:05:25 +0200

    updated

--- src/main/java/vs/jonas/services/services/DiceService.java
+++ src/main/java/vs/jonas/services/services/DiceService.java
@@ -39,7 +39,7 @@ public class DiceService {
 			
 			// Wenn die Player-Uri und die GameUri angegeben wurde, kann ein Event erstellt werden:
 			if((playerUri != null && !playerUri.equals(""))&& ( gameUri != null && !gameUri.equals(""))){
-				// EventData(String game, String type, String name, String reason, String ressource, String player)
+				
 				EventData event = new EventData(gameUri,"DiceRoll","DiceRoll",playerUri+" has rolled the dice.", SLASH_DICE,playerUri);
 				RestClient client = new RestClient();
 				Service service = neededServices.get(ServiceNames.EVENT);

08e1e4a7483b539bfa9116c717278118a335af6e
Gerriet Hinrichs <gerriet.hinrichs@web.de>
2016-05-26 18:45:43 +0200

    Fixed yellow pages uri, added proper marshaling

--- src/main/java/vs/gerriet/api/ApiBase.java
+++ src/main/java/vs/gerriet/api/ApiBase.java
@@ -1,11 +1,37 @@
 package vs.gerriet.api;
 
+import com.google.gson.Gson;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
 /**
  * Base API class.
  *
  * @author Gerriet Hinrichs {@literal <gerriet.hinrichs@web.de>}
  */
 public abstract class ApiBase {
+
+    /**
+     * Initialize object mapper for unirest.
+     */
+    static {
+        Unirest.setObjectMapper(new ObjectMapper() {
+            /**
+             * Gson instance for marshaling.
+             */
+            private final Gson gson = new Gson();
+
+            @Override
+            public <T> T readValue(final String value, final Class<T> valueType) {
+                return this.gson.fromJson(value, valueType);
+            }
+
+            @Override
+            public String writeValue(final Object value) {
+                return this.gson.toJson(value);
+            }
+        });
+    }
 
     /**
      * Returns the service uri.

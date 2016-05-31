295c7a7a87e4296ae4b17234fb03025c0ed81851
jnk111 <dieckhoff.jan@gmx.de>
2016-05-31 17:19:10 +0200

    added ne run method

--- src/main/java/vs/jan/services/run/boardservice/RunBoardService.java
+++ src/main/java/vs/jan/services/run/boardservice/RunBoardService.java
@@ -1,44 +1,10 @@
 package vs.jan.services.run.boardservice;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import vs.gerriet.service.BankService;
 import vs.jan.api.boardservice.BoardRESTApi;
-import vs.jan.api.userservice.UserServiceRESTApi;
-import vs.jan.json.boardservice.JSONService;
-import vs.jan.model.ServiceNames;
-import vs.jonas.services.services.DiceService;
-import vs.jonas.services.services.EventService;
-import vs.malte.services.GamesService;
 
 public class RunBoardService {
 
 	public static void main(String[] args) {
-		BankService.run();
-		Map<String, JSONService> neededServicesDice = getNeededServices(ServiceNames.DICE);
-		new EventService().startService(); // Der EventService muss f�r den
-																				// DiceService laufen
-		new GamesService();
-		new DiceService(neededServicesDice).startService();
-		new UserServiceRESTApi();
-		// new GameServiceFixed();
 		new BoardRESTApi();
-	}
-
-	private static Map<String, JSONService> getNeededServices(String type) {
-		Map<String, JSONService> services = new HashMap<>();
-
-		// services.put(ServiceNames.EVENT, start.getService(ServiceNames.EVENT));
-		// ... weitere
-
-		if (type.equals(ServiceNames.DICE) || type.equals(ServiceNames.BOARD)) {
-			JSONService s = new JSONService("/services/13", "Logs the Events", "bla", ServiceNames.EVENT, "running",
-					"http://localhost:4567/events");
-
-			services.put(ServiceNames.EVENT, s);
-		}
-
-		return services;
 	}
 }

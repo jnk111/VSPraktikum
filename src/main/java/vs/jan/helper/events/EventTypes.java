47a29579fc380fd844bc5bdd6beb44eeb025728a
jnk111 <dieckhoff.jan@gmx.de>
2016-06-15 18:53:24 +0200

    added mutex changed and game started event types

--- src/main/java/vs/jan/helper/events/EventTypes.java
+++ src/main/java/vs/jan/helper/events/EventTypes.java
@@ -8,7 +8,7 @@ public enum EventTypes {
 							"cannotdeletehypothecary"), MOVED_TO_JAIL("movedtojail"), TRADE_PLACE("tradeplace"), CANNOT_TRADE_PLACE(
 									"cannottradeplace"), GOT_MONEY_ALL_PLAYERS("gotmoneyallplayers"), GOT_MONEY_FROM_BANK(
 											"got money from bank"), MOVED_OVER_GO("movedovergo"), CANNOT_PAY_MONEY_COMMUNITY(
-													"cannotpaymoney");
+													"cannotpaymoney"), MUTEX_CHANGE("mutexchanged"), GAME_STARTED("gamestarted");
 
 	private String type;
 

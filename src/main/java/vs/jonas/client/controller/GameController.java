f7b5161d6f938f35a9a57042f99a940a830972d5
jnk111 <dieckhoff.jan@gmx.de>
2016-06-16 11:58:06 +0200

    fix

--- src/main/java/vs/jonas/client/controller/GameController.java
+++ src/main/java/vs/jonas/client/controller/GameController.java
@@ -72,8 +72,8 @@ public class GameController {
 		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 		String ipadress = br.readLine();
 		this.ip = ipadress;//InetAddress.getLocalHost().getHostAddress();
-		 this.ip = "0.0.0.0";
-//		this.ip = "192.168.255.18";
+		 //this.ip = "0.0.0.0";
+		this.ip = "192.168.255.18";
 		
 		this.user.setUri("http://" + this.ip + ":" +this.PORT + SLASH_CLIENT + "/" + user.getName());
 		startClientService();
